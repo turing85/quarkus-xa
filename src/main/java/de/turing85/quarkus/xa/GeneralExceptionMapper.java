d261ca8603849641956f40b2bddb79b729e41425
Marco Bungart <marco.bungart@googlemail.com>
2024-12-23 09:36:08 +0100

    Simplify exception mapping

--- src/main/java/de/turing85/quarkus/xa/GeneralExceptionMapper.java
+++ src/main/java/de/turing85/quarkus/xa/GeneralExceptionMapper.java
@@ -1,23 +1,21 @@
 package de.turing85.quarkus.xa;
 
-import java.nio.charset.StandardCharsets;
-
-import jakarta.ws.rs.core.HttpHeaders;
+import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.core.MediaType;
 import jakarta.ws.rs.core.Response;
 import jakarta.ws.rs.ext.ExceptionMapper;
 import jakarta.ws.rs.ext.Provider;
 
 @Provider
+@Produces(MediaType.APPLICATION_JSON)
 public class GeneralExceptionMapper implements ExceptionMapper<Exception> {
-  public static final String BODY = "{ \"message\": \"internal server error\"}";
+  public static final String BODY = "{\"message\": \"internal server error\"}";
 
   @Override
   public Response toResponse(Exception exception) {
     // @formatter:off
-    return Response.status(Response.Status.INTERNAL_SERVER_ERROR.getStatusCode())
-        .header(HttpHeaders.CONTENT_TYPE,
-            "%s;charset=%s".formatted(MediaType.APPLICATION_JSON, StandardCharsets.UTF_8))
+    return Response
+        .status(Response.Status.INTERNAL_SERVER_ERROR.getStatusCode())
         .entity(BODY)
         .build();
     // @formatter:on
